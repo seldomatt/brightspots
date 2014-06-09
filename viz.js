0ec3258eb36ea3d4e87a8e1b6f3bc9787c2d14cd
Jessica Simon and Matt Salerno <zephyr-eng+jessica+matt@gust.com>
2014-06-09 16:22:22 -0400

    Working

--- viz.js
+++ viz.js
@@ -31,14 +31,8 @@ $(document).ready(function() {
     .enter()
     .append("circle")
     .attr("class", function(d){return setClass(d);})
-    .style("fill",function(d){return d.children ? color(d.depth) : null;})
-    .on("click", function(d){if (focus !== d) zoom(d), showCircles(d), d3.event.stopPropagation();})
-    // .on("mouseover", function(d){ showCircles(d) } )
-
-
-    var text= svg.selectAll("text")
-
-
+    .style("fill",function(d){return d.children ? color(d.depth) : '#BDFCFF';})
+    .on("click", function(d){ if (focus !== d) zoom(d), d3.event.stopPropagation();})
 
     function setClass(d) {
       if (d.parent) {
@@ -58,86 +52,85 @@ $(document).ready(function() {
       }
     }
 
-    function showCircles(d) {
-      if (d.depth >= 1) {
-        $(".node--leaf").css( { 'fill': 'white', 'display': 'block' } )
-        $(".node--middle").css( { 'fill': 'blue', 'fill-opacity': '.5', 'display': 'block' } )
-      }
-      else {
-        $(".node--leaf").css("display", "none")
-      }
-
-    }
-
-
-    function inspectChildren(d) {
-     for(var i = 0; i < d.children.length; i++){
-        if (d.children[i].children) {
-          return  "node--top"
-        } else {
-          return "node--middle"
-        }
-     }
-    }
-
-
     var text= svg.selectAll("foreignObject")
     .data(nodes)
     .enter().append("foreignObject")
     .html(function(d){return d.name})
     .attr("class","label")
     .style("fill-opacity", function(d) {return d.parent === tree ? 1 :0; })
-    .style("display",function(d){return d.parent === tree ? null : "none";});
-    // .text(function(d){ return d.name; });
+    .style("text-align", "center")
+    .style("display",function(d){return d.parent === tree ? "inline" : "none";});
 
     var text_nodes= svg.selectAll(".label")
     var circle_nodes= svg.selectAll("circle")
 
     d3.select("body")
     .style("background",color(-1))
-    .on("click",function(){zoom (tree);});
+    .on("click",function(){backgroundZoom(tree);});
 
     zoomTo([tree.x,tree.y,tree.r*2  + margin])
 
-    function zoom(d){
+    function backgroundZoom(d) {
       var focus0 = focus;
       var focus =d;
 
       var transition = d3.transition()
-      .duration(1000)
+      .duration(700)
       .tween("zoom",function(d){
         var i= d3.interpolateZoom(view,[focus.x,focus.y,focus.r *2 + margin]);
         return function (t){zoomTo(i(t));};
       });
+
+      console.log("background Zoom");
+      transition.selectAll("circle")
+      .filter(function(d){ return d.parent ===focus || this.style.display === "inline";})
+      .style("fill-opacity", function(d) {return (d.parent === focus) ? 1:0})
+      .each("end",function(d){if (d.parent !== focus ) this.style.display = "none";})
 
       transition.selectAll(".label")
       .filter(function(d){return d.parent ===focus || this.style.display === "inline";})
       .style("fill-opacity", function(d) {return d.parent === focus ? 1:0})
       .each("start",function(d){if (d.parent === focus) this.style.display = "inline";})
       .each("end",function(d){if (d.parent !== focus) this.style.display = "none";})
+    }
+
+    function zoom(d){
+      var focus0 = focus;
+      var focus =d;
+
+      var transition = d3.transition()
+      .duration(700)
+      .tween("zoom",function(d){
+        var i= d3.interpolateZoom(view,[focus.x,focus.y,focus.r *2 + margin]);
+        return function (t){zoomTo(i(t));};
+      });
+
+      console.log("Zoom");
+      transition.selectAll("circle")
+      .filter(function(d){ return d.parent ===focus || this.style.display === "inline";})
+      .style("fill-opacity", function(d) {return (d.parent === focus) ? 1:0})
+      .each("start",function(d){if (d.parent === focus) this.style.display = "inline";})
+      .each("end",function(d){if (d.parent !== focus ) this.style.display = "none";})
+
+      transition.selectAll(".label")
+      .filter(function(d){return d.parent ===focus || this.style.display === "inline";})
+      .style("fill-opacity", function(d) {return d.parent === focus ? 1:0})
+      .each("start",function(d){if (d.parent === focus) this.style.display = "inline";})
       .each("end",function(d){if (d.parent !== focus) this.style.display = "none";})
     }
 
     function zoomTo(v){
-      console.log(circle)
+
       var k = diameter /v[2]; view =v;
       circle_nodes.attr("transform", function (d){ return "translate(" + (d.x-v[0]) * k + "," + (d.y - v[1]) * k + ")" })
       circle.attr("r",function(d){return d.r *k});
-      text_nodes.attr("width", function(d) {return d.r *k *1.8});
-      text_nodes.attr("height", function(d) {return d.r *k *1.8});
-      text_nodes.attr("x", function(d) { return ((d.x-v[0]) * k) - (d.r *k) + 8 });
-      text_nodes.attr("y", function(d) { return (d.y - v[1]) * k });
+      text_nodes.attr("width", function(d) {return d.r *k *2});
+      text_nodes.attr("height", function(d) {return d.r *k *2});
+      text_nodes.attr("x", function(d) { return ((d.x-v[0]) * k) - (d.r *k) });
+      text_nodes.attr("y", function(d) { return ((d.y - v[1]) * k) -15});
     }
-
-
-
 
     d3.select(self.frameElement).style("height",diameter + "px");
 
-
-
-
   });
-  //d3.json('flare.json', function(tree){
-
 });
